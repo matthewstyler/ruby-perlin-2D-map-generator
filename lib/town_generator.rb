07c5a6713e30ba7edcc1660921b923b50bad08b2
matthewstyer <matthews.tyl@gmail.com>
2023-08-11 10:36:29 -0400

    connect towns by roads

--- lib/town_generator.rb
+++ lib/town_generator.rb
@@ -18,10 +18,12 @@ class TownGenerator
 
     seed = config.town_seed
 
-    (1..config.towns).each do |_n|
-      generate_random_town(seed)
+    all_town_points = (1..config.towns).map do |_n|
+      town_points = generate_random_town(seed)
       seed += 1000
+      town_points
     end
+    generate_roads_between_towns(all_town_points)
   end
 
   private
@@ -38,6 +40,7 @@ class TownGenerator
       point.add_town_item(seed)
     end
     generate_town_roads(points)
+    points
   end
 
   def generate_town_roads(points)
@@ -49,7 +52,47 @@ class TownGenerator
         road_generator.generate_roads_from_coordinate_list([point_one.x, point_one.y, point_two.x, point_two.y])
 
         connected_pairs.add([point_one, point_two])
+        connected_pairs.add([point_two, point_one])
       end
     end
+  end
+
+  def generate_roads_between_towns(all_town_points)
+    return if all_town_points.length < 2
+
+    connected_pairs = Set.new
+    town_centroids = {}
+
+    all_town_points.each_with_index do |town_one, idx_one|
+      find_town_centroid(town_one)
+
+      all_town_points[idx_one + 1..].each do |town_two|
+        next if connected_pairs.include?([town_one, town_two]) || connected_pairs.include?([town_two, town_one])
+
+        town_one_center_x, town_one_center_y = (town_centroids[town_one] ||= find_town_centroid(town_one))
+        town_two_center_x, town_two_center_y = (town_centroids[town_two] ||= find_town_centroid(town_two))
+
+        road_generator.generate_roads_from_coordinate_list([town_one_center_x, town_one_center_y, town_two_center_x, town_two_center_y])
+
+        connected_pairs.add([town_one, town_two])
+        connected_pairs.add([town_two, town_one])
+      end
+    end
+  end
+
+  def find_town_centroid(points)
+    total_x = 0
+    total_y = 0
+    num_coordinates = points.length
+
+    points.each do |point|
+      total_x += point.x
+      total_y += point.y
+    end
+
+    average_x = total_x / num_coordinates.to_f
+    average_y = total_y / num_coordinates.to_f
+
+    [average_x, average_y]
   end
 end
